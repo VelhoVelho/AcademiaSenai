4b914d0e2629375e6b9f8d0330147bd0999fb5cf
Paulo Ricardo <velhopaulo891@gmail.com>
2022-03-17 19:50:30 -0300

    inicio do desenvolto dos recursos de gestão de instrutores e equipamentos

---
+++ src/br/senai/academiasenai/Equipamento.java
@@ -0,0 +1,5 @@
+package br.senai.academiasenai;
+
+public class Equipamento {
+
+}
---
+++ src/br/senai/academiasenai/InitApp.java
@@ -0,0 +1,10 @@
+package br.senai.academiasenai;
+
+public class InitApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
